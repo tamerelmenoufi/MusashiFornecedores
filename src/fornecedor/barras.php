e73726aa9dae31a92b7347530f3568dbbb945f73
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-23 17:27:26 -0400

    Projeto MUSASHI Fornecedores

--- src/fornecedor/barras.php
+++ src/fornecedor/barras.php
@@ -80,21 +80,6 @@
             return 0;
         }
     }
-    echo "SELECT f.nome,
-    am.mes,
-    am.ano,
-    am.eficiencia,
-    am.quality,
-    am.delivery,
-    am.classificacao,
-    am.posicao,
-    am.*
-    FROM `avaliacao_mensal` am
-    LEFT JOIN fornecedores f ON am.codigo_fornecedor = f.codigo
-    where f.codigo = {$_POST['codigo']}
-    and am.ano between ('{$ano_atual}' AND '{$Y}') ORDER BY am.ano DESC, am.mes ASC LIMIT 12";
-
-    exit();
 
     $query = $pdo->prepare("SELECT f.nome,
     am.mes,
@@ -108,7 +93,7 @@
     FROM `avaliacao_mensal` am
     LEFT JOIN fornecedores f ON am.codigo_fornecedor = f.codigo
     where f.codigo = {$_POST['codigo']}
-    and am.ano between ('{$ano_atual}' AND '{$Y}') ORDER BY am.ano DESC, am.mes ASC LIMIT 12");
+    and am.ano between '{$ano_atual}' AND '{$Y}' ORDER BY am.ano DESC, am.mes ASC LIMIT 12");
     $query->execute();
 
     $array_valores = [];
