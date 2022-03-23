83d9e70856fcb70d4ce84cbc3c131a6894ce8229
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-23 13:27:26 -0400

    Projeto MUSASHI Fornecedores

--- src/fornecedor/actions/mes_action.php
+++ src/fornecedor/actions/mes_action.php
@@ -1,6 +1,6 @@
 <?php
     require_once "../../../lib/config.php";
-    
+
     global $pdo;
 
     $m = date("m", strtotime($_POST["data"]));
@@ -34,7 +34,12 @@
         $update = $pdo->prepare("UPDATE avaliacao_mensal SET status = '1' WHERE codigo = {$mes['codigo']}");
         $update->execute();
 
-        $medias = $pdo->prepare("SELECT AVG(eficiencia) as eficiencia, AVG(100 - total_demerito_delivery) as delivery, AVG(100 - total_demerito_quality) as quality, (AVG(eficiencia) + AVG(100 - total_demerito_delivery) + AVG(100 - total_demerito_quality))/3 as classificacao FROM registros_diarios 
+        $medias = $pdo->prepare("SELECT
+                                        AVG(eficiencia) as eficiencia,
+                                        AVG(100 - total_demerito_delivery) as delivery,
+                                        AVG(100 - total_demerito_quality) as quality,
+                                        (AVG(eficiencia) + AVG(100 - total_demerito_delivery) + AVG(100 - total_demerito_quality))/3 as classificacao
+                                FROM registros_diarios
         WHERE codigo_fornecedor = {$_POST['codigo_fornecedor']} AND year(data_registro) = {$Y} AND status = 1");
 
         $medias->execute();
@@ -82,11 +87,12 @@
         qualificacao_ipf = '{$q_ipf}',
         qualificacao_iqf = '{$q_iqf}',
         qualificacao_iaf = '{$q_iaf}'
-        WHERE codigo_fornecedor = {$_POST['codigo_fornecedor']} 
+        WHERE codigo_fornecedor = {$_POST['codigo_fornecedor']}
         AND mes = {$m} AND ano = {$Y}");
 
         $update->execute();
 
+        //Inclusão das posições Geral
         $max = $pdo->prepare("SELECT codigo, classificacao, ano, mes, posicao FROM avaliacao_mensal
         WHERE mes = {$m} AND ano = {$Y} ORDER BY classificacao DESC");
         $max->execute();
@@ -110,5 +116,60 @@
             }
             $p++;
         }
+
+        //Inclusão das posições Quality
+        $max = $pdo->prepare("SELECT codigo, quality, ano, mes, posicao_quality FROM avaliacao_mensal
+        WHERE mes = {$m} AND ano = {$Y} ORDER BY quality DESC");
+        $max->execute();
+
+        $pQ = 1;
+        $ppQ = 0;
+        while($d = $max->fetch()){
+            if($pQ == 1){
+                $update = $pdo->prepare("UPDATE avaliacao_mensal SET posicao_quality = 1 WHERE codigo = {$d['codigo']}");
+                $update->execute();
+
+                $ppQ = $d['quality'];
+            }
+
+            if($d['quality'] == $ppQ){
+                $update = $pdo->prepare("UPDATE avaliacao_mensal SET posicao_quality = 1 WHERE codigo = {$d['codigo']}");
+                $update->execute();
+            }else{
+                $update = $pdo->prepare("UPDATE avaliacao_mensal SET posicao_quality = {$pQ} WHERE codigo = {$d['codigo']}");
+                $update->execute();
+            }
+            $pQ++;
+        }
+
+
+        //Inclusão das posições Delivery
+        $max = $pdo->prepare("SELECT codigo, delivery, ano, mes, posicao_delivery FROM avaliacao_mensal
+        WHERE mes = {$m} AND ano = {$Y} ORDER BY delivery DESC");
+        $max->execute();
+
+        $pD = 1;
+        $ppD = 0;
+        while($d = $max->fetch()){
+            if($pD == 1){
+                $update = $pdo->prepare("UPDATE avaliacao_mensal SET posicao_delivery = 1 WHERE codigo = {$d['codigo']}");
+                $update->execute();
+
+                $ppD = $d['delivery'];
+            }
+
+            if($d['delivery'] == $ppD){
+                $update = $pdo->prepare("UPDATE avaliacao_mensal SET posicao_delivery = 1 WHERE codigo = {$d['codigo']}");
+                $update->execute();
+            }else{
+                $update = $pdo->prepare("UPDATE avaliacao_mensal SET posicao_delivery = {$pD} WHERE codigo = {$d['codigo']}");
+                $update->execute();
+            }
+            $pD++;
+        }
+
+
+
+
     }
 ?>
