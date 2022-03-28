dba98b5f00329b7b30c66795c364901babaf5523
Mateus de Lucena <mateustavares@protonmail.com>
2022-03-27 23:27:13 -0400

    Alteração QAV
    QAV alterado para Avaliação Mensal

--- src/fornecedor/actions/qav_action.php
+++ src/fornecedor/actions/qav_action.php
@@ -4,19 +4,31 @@
     global $pdo;
 
     $Y = $_POST['ano'];
+    $M = $_POST['mes'];
 
     if($_POST['qav'] && ($_POST['qav'] > 0 || $_POST['qav'] != '')){
-        $sql = $pdo->prepare("SELECT * FROM avaliacao_anual WHERE codigo_fornecedor = :cf AND ano = :y");
+        $sql = $pdo->prepare("SELECT * FROM avaliacao_mensal WHERE codigo_fornecedor = :cf AND ano = :y AND mes = :m");
+
+        echo "SELECT * FROM avaliacao_mensal WHERE codigo_fornecedor = {$_POST['codigo_fornecedor']} AND ano = {$Y} AND mes = {$M}";
+        
         $sql->bindValue(":cf", $_POST['codigo_fornecedor']);
         $sql->bindValue(":y", $Y);
+        $sql->bindValue(":m", $M);
         $sql->execute();
 
         if($sql->rowCount() > 0){
-            $update = $pdo->prepare("UPDATE avaliacao_anual SET qav = :qv, qav_data = NOW() WHERE codigo_fornecedor = :cf
-            AND ano = :y");
+            $update = $pdo->prepare("UPDATE avaliacao_mensal SET qav = :qv, qav_data = NOW() WHERE codigo_fornecedor = :cf
+            AND ano = :y
+            AND mes = :m");
             $update->bindValue(":qv", $_POST['qav']);;
             $update->bindValue(":cf", $_POST['codigo_fornecedor']);
             $update->bindValue(":y", $Y);
+            $update->bindValue(":m", $M);
+
+            echo "UPDATE avaliacao_mensal SET qav = {$_POST['qav']}, qav_data = NOW() WHERE codigo_fornecedor = {$_POST['codigo_fornecedor']}
+            AND ano = {$Y}
+            AND mes = {$M}";
+
 
             $update->execute();
         }
--- src/fornecedor/relatorio_fornecedor.php
+++ src/fornecedor/relatorio_fornecedor.php
@@ -242,16 +242,18 @@
 
         <div class="row m-0 p-0 justify-content-center ">
             <?php
-                $sql = $pdo->prepare("SELECT * FROM avaliacao_anual WHERE codigo_fornecedor = :cf AND ano = :y AND status = 1");
+                $sql = $pdo->prepare("SELECT * FROM avaliacao_mensal WHERE codigo_fornecedor = :cf AND ano = :y  AND mes = :m AND status = 1");
                 $sql->bindValue(":cf", $_POST['codigo_fornecedor']);
                 $sql->bindValue(":y", $Y);
+                $sql->bindValue(":m", $M);
                 $sql->execute();
 
                 if($sql->rowCount()){
                     $pontuacao = $sql->fetch();
 
-                    $query = $pdo->prepare("SELECT count(codigo) as quantidade FROM avaliacao_anual WHERE ano = :y AND status = 1");
+                    $query = $pdo->prepare("SELECT count(codigo) as quantidade FROM avaliacao_mensal WHERE ano = :y  AND mes = :m AND status = 1");
                     $query->bindValue(":y", $Y);
+                    $query->bindValue(":m", $M);
                     $query->execute();
 
                     $qnt = $query->fetch();
