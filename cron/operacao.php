e88fee31dd83cb308718203fabfdc62bf3f4f5ec
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-17 00:01:26 -0400

    Atualizações Ajustes recalculo

--- cron/operacao.php
+++ cron/operacao.php
@@ -3,14 +3,12 @@
 
     global $pdo;
 
-    $sqlCron = $pdo->prepare("SELECT * FROM `registros_diarios` order by data_registro asc limit 5");
+    $sqlCron = $pdo->prepare("SELECT * FROM `registros_diarios` order by data_registro asc");
     $sqlCron->execute();
     while($dCron = $sqlCron->fetch()){
         set_time_limit(90);
-        echo "---------------------------------------------------------------------------------";
         echo $dCron['codigo_fornecedor'].' - '.$dCron['data_registro']."<br>";
         include('mes_action.php');
-        echo "---------------------------------------------------------------------------------";
     }
 
 ?>
