88a6d4659263591895deb3a4865ad86ec6f9f439
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-26 08:35:58 -0400

    Atualizações

--- src/pages/graphics/pie_geral.php
+++ src/pages/graphics/pie_geral.php
@@ -44,7 +44,7 @@
                 ava.posicao
                 FROM avaliacao_mensal ava
                 LEFT JOIN fornecedores f ON ava.codigo_fornecedor = f.codigo
-                WHERE ava.ano = '{$Ano}' AND ava.mes = '{$Mes}' ORDER BY ava.classificacao DESC");
+                WHERE ava.ano = '{$Ano}' AND ava.mes = '{$Mes}' GROUP BY ava.codigo_fornecedor");
         $query->execute();
 
         while($d = $query->fetch()) {
