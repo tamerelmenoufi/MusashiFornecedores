25aa608d1231c0f778c6a3883da3335c9cb33a2f
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-28 21:48:39 -0400

    Atualizações

--- src/fornecedor/relatorio/IAF/linhas.php
+++ src/fornecedor/relatorio/IAF/linhas.php
@@ -260,7 +260,7 @@ for($i=11; $i>=0; $i--){
     $d = $query->fetch();
 
     $ind = ($Mes*1);
-    $retorno = dias_atrasos($Mes, $Ano, $_POST['codigo_fornecedor']);
+    $retorno = dias_atrasos($Mes, $Ano, $_POST['codigo']);
     $array_meses[$ind] =  '"'.mesExtenso($ind).'/'.substr($Ano,-2).'"';
     $entrega[$ind] = $retorno['delivery'];
     $atraso[$ind] = $retorno['pct_atendimento'];
