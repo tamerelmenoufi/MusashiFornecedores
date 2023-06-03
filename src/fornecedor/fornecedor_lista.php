11d11527b6468f886d261bc4fa9dde1773385cfa
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-03 13:09:44 +0300

    Atualizações

--- src/fornecedor/fornecedor_lista.php
+++ src/fornecedor/fornecedor_lista.php
@@ -44,7 +44,7 @@
                         <?php
                             $data_atual = date("Y-m-d");
 
-                            $sql = $pdo->prepare("SELECT * FROM fornecedores WHERE '{$data_atual}' BETWEEN data_inicio AND data_fim /*AND deletado != '1'*/ ORDER BY deletado asc nome desc;");
+                            $sql = $pdo->prepare("SELECT * FROM fornecedores WHERE '{$data_atual}' BETWEEN data_inicio AND data_fim /*AND deletado != '1'*/ ORDER BY deletado asc, nome desc");
                             $sql->execute();
                             $count = 1;
                             while($d = $sql->fetch()){
