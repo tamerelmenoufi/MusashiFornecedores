3e5dc0e3ad124f0053607a0475f874bfaa34d409
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-13 06:39:20 -0500

    Atualizações

--- src/pages/assinaturas.php
+++ src/pages/assinaturas.php
@@ -6,7 +6,7 @@ global $pdo;
 <div class="m-3" style="padding-top:20px;">
     <h4>Gestão de Assinaturas</h4>
     <p>Módulo para ordenar a sequência das assinaturas no sistema.</p>
-    <select id="documentos" class="form-control">
+    <select id="documentos" class="form-select">
         <option value="">:: Selecione o documento::</option>
         <option value="doc_ipf">Relatório IPF</option>
         <option value="doc_iqf">Relatório IQF</option>
