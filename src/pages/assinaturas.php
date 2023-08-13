5bd2ad338ba6a18d1042813840e576aefda112be
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-13 06:16:29 -0500

    Atualizações

--- src/pages/assinaturas.php
+++ src/pages/assinaturas.php
@@ -3,7 +3,7 @@ require "../../lib/config.php";
 
 global $pdo;
 ?>
-<div class="m-3" style="margin-top:90px;">
+<div class="m-3" style="padding-top:20px;">
     <h4>Gestão de Assinaturas</h4>
     <p>Módulo para ordenar a sequência das assinaturas no sistema.</p>
 </div>
