bddccab6401069540d66e08e9db786087d7f098c
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-09 20:01:48 +0300

    Atualizações

--- cron/assinaturas.php
+++ cron/assinaturas.php
@@ -4,3 +4,29 @@
     global $pdo;
 
     $query = "select *, min(ordem) as maximo from assinaturas where status != '1' group by codigo_avaliacao_mensal, doc order by codigo asc";
+    $result = mysqli_query($con, $query);
+    wheile($d = mysqli_fetch_object($result)){
+
+    }
+
+    $html = file_get_contents("./alert.html");
+
+
+    $dados = [
+        'from_name' => 'Mailgun - MOHATRON',
+        'from_email' => 'mailgun@moh1.com.br',
+        'subject' => 'Nusashi Fornecedores - Alerta de Assinaturas',
+        'html' => $html,
+        // 'attachment' => [
+        //         './img_bk.png',
+        //         './cliente-mohatron.xls',
+        //         './formulario_prato_cheio.pdf',
+        // ],
+        'inline' => [
+                'http://musashi.mohatron.com/img/banner_notificacao.png',
+        ],
+        'to' => [
+                ['to_name' => 'Tamer Elmenoufi', 'to_email' => 'tamer.menoufi@gmail.com'],
+                // ['to_name' => 'Tamer Mohamed', 'to_email' => 'tamer@mohatron.com.br'],
+        ]
+ ];
