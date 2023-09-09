168009362707f9875defd73a742739d3f1e47b71
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-09 20:21:28 +0300

    Atualizações

--- cron/assinaturas.php
+++ cron/assinaturas.php
@@ -29,4 +29,23 @@
                 ['to_name' => 'Tamer Elmenoufi', 'to_email' => 'tamer.menoufi@gmail.com'],
                 // ['to_name' => 'Tamer Mohamed', 'to_email' => 'tamer@mohatron.com.br'],
         ]
- ];
+    ];
+
+    $url = "http://email.mohatron.com/send.php";
+    // Make a POST request
+    $options = stream_context_create(['http' => [
+            'method'  => 'POST',
+            'header' => 'Content-Type: application/x-www-form-urlencoded',
+            'content' => http_build_query($dados)
+        ]
+    ]);
+
+    // Send a request
+    $result = file_get_contents($url, false, $options);
+    $result = json_decode($result);
+
+    // echo "<pre>";   
+    // print_r($result);
+    // echo "</pre>";
+
+    return $result->status;
