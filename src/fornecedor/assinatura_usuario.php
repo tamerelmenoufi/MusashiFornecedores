cdcc18e9960736b0b025ea4e3d03b3e38a5387e9
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-18 19:37:37 +0300

    Atualizações

--- src/fornecedor/assinatura_usuario.php
+++ src/fornecedor/assinatura_usuario.php
@@ -64,39 +64,21 @@ if ($_POST['acao'] === 'assinar') {
 
         if ($query3->execute()) {
 
-            $TipoDoc = [
-                'assinaturas_ipf' => 'doc_ipf',
-                'assinaturas_iqf' => 'doc_iqf',
-                'assinaturas_iaf' => 'doc_iaf',
-            ];
-
             $query4 = $pdo->prepare("UPDATE assinaturas SET 
                                                 status = '1'
                                             WHERE 
-                                                doc = '{$TipoDoc[$campo_assinatura]}' and 
-                                                usuario = '{$_SESSION['musashi_cod_usu']}' and 
-                                                codigo_avaliacao_mensal = '{$avaliacao_mes['codigo']}'
+                                                codigo = '{$_POST['cod_assinatura']}'
                                     ");
             $query4->execute();
 
-            echo json_encode([
-                "status"     => true,
-                "msg"        => "Assinado com sucesso",
-                "codigo"     => $avaliacao_mes['codigo'],
-                "dados"      => $nova_assinatura,
-                "cod_mensal" => $_SESSION['cod_mensal'],
-                "tipo"       => $ConfUsu['tipo']
-            ]);
+        } 
 
-            unset($_SESSION['cod_mensal']);
-        } else {
-            echo json_encode([
-                "status" => false,
-                "msg"    => "Erro ao inserir assinatura",
-                "error"  => $query3->errorInfo()
-            ]);
-        }
-        #@formatter:on
+
+        echo json_encode([
+            "status" => true,
+            "msg" => "Assinatura realizada com sucesso!"
+        ]);
+
     } else {
         echo json_encode([
             "status" => false,
@@ -128,6 +110,8 @@ if ($_POST['acao'] === 'assinar') {
         <?php
 
         $sql = $pdo->prepare("select 
+        a.codigo,
+        a.codigo_avaliacao_mensal,
         a.usuario,
         a.doc,
         b.nome,
@@ -150,7 +134,12 @@ if ($_POST['acao'] === 'assinar') {
 ?>
                         <tr>
                             <td>
-                                <input type="checkbox" />
+                                <input 
+                                    opcoes
+                                    tipo_relatorio="<?=$doc[$d['doc']]?>"
+                                    cod_mensal="<?=$d['codigo_avaliacao_mensal']?>"
+                                    cod_assinatura="<?=$d['codigo']?>"
+                                    type="checkbox" />
                             </td>
                             <td><?=$i?></td>
                             <td><?=$doc[$d['doc']]?></td>
@@ -164,6 +153,9 @@ if ($_POST['acao'] === 'assinar') {
 ?>
                     </tbody>
                 </table>
+
+                <button assinar class="btn btn-success m-3">ASSINAR RELATÓRIO(S)</button>
+
             </div>
         </div>
     </div>
@@ -171,6 +163,22 @@ if ($_POST['acao'] === 'assinar') {
 
 <script>
     $(function () {
-        
+        $("button[assinar]").click(function(){
+
+            
+            tipo_relatorio = [];
+            cod_mensal = [];
+            cod_assinatura = [];             
+            $("input[opcoes]").each(function(){
+                tipo_relatorio.push($(this).attr("tipo_relatorio"));
+                cod_mensal.push($(this).attr("cod_mensal"));
+                cod_assinatura.push($(this).attr("cod_assinatura"));                
+            })
+
+            console.log(tipo_relatorio)
+            console.log(cod_mensal)
+            console.log(cod_assinatura)
+
+        })
     });
 </script>
